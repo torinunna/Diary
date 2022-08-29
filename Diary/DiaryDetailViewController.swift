e4fd24b2c62b90ba2290f600f5cb33601133e007
torinunna <torinunna9588@gmail.com>
2022-08-29 23:57:26 +0900

    create DiaryDetailVC, WriteDiaryVC

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by YUJIN KWON on 2022/08/29.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by YUJIN KWON on 2022/08/29.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+
+}
