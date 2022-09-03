754318ae47f2f8c33c69b01389cce5cff9f345cb
torinunna <torinunna9588@gmail.com>
2022-09-03 23:58:57 +0900

    add IBOutlets and IBActions  to DiaryDetail

--- Diary/DiaryDetailViewController.swift
+++ Diary/DiaryDetailViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class DiaryDetailViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentsTextview: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+  
+    var diary: Diary?
+    var indexPath: IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+    }
+    
+    @IBAction func editPressed(_ sender: UIButton) {
+    }
+    @IBAction func deletePressed(_ sender: UIButton) {
     }
     
 
