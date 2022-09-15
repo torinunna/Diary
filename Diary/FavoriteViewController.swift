cb5426aa272ec6a741b422529812452068c802f9
torinunna <torinunna9588@gmail.com>
2022-09-15 16:59:55 +0900

    add favorite diary to FavoriteCell

--- Diary/FavoriteViewController.swift
+++ Diary/FavoriteViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class FavoriteViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var diaryList = [Diary]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configureCollectionView()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.loadFavoriteList()
+    }
+    
+    private func configureCollectionView() {
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    private func dateToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+
+    private func loadFavoriteList() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        self.diaryList = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isFavorite = $0["isFavorite"] as? Bool else { return nil}
+            return Diary(title: title, contents: contents, date: date, isFavorite: isFavorite)
+        }.filter({
+            $0.isFavorite == true
+        }).sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+        self.collectionView.reloadData()
     }
  
 
+}
+
+extension FavoriteViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.diaryList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteCell", for: indexPath) as? FavoriteCell else { return UICollectionViewCell()}
+        let diary = self.diaryList[indexPath.row]
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = self.dateToString(date: diary.date)
+        return cell
+    }
+}
+
+extension FavoriteViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 20, height: 80)
+    }
 }
